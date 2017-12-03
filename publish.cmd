e5b4873b4ffd22466ba7e40b7acbbd5717f1652b
maduka <maduka_bor@hotmail.com>
2017-12-03 19:05:16 +0800

    加入專案檔案。

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=madukabotservice-Web-Deploy.pubxml -p:Password=KfDaKGgsEj9ejZTYhoPm6AYzY0ofpvFbcW6uHAkYYeC7ZvcvKAMoMzz70mBz
+
