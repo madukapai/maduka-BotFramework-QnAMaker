d14f086764659a7b94a855bfcea0f9a58bb8e89b
maduka <maduka_bor@hotmail.com>
2017-12-03 21:05:55 +0800

    加上連絡小編的功能

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -8,12 +8,15 @@ using System.Web.Http.Description;
 using System.Net.Http;
 using System.Diagnostics;
 using System.Collections.Generic;
+using System.Runtime.Caching;
 
 namespace Microsoft.Bot.Sample.QnABot
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+        MemoryCache memoryCache = MemoryCache.Default;
+
         /// <summary>
         /// POST: api/Messages
         /// receive a message from a user and send replies
@@ -22,17 +25,22 @@ namespace Microsoft.Bot.Sample.QnABot
         [ResponseType(typeof(void))]
         public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
         {
-            // check if activity is of type message
             ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
 
-            // check if activity is of type message
-            if (activity.GetActivityType() == ActivityTypes.Message)
-            {
-                await Conversation.SendAsync(activity, () => new BasicQnAMakerDialog());
-            }
+            // 判斷是否為直接連絡真人的清單
+           var strIsConnect = memoryCache.Get(activity.Conversation.Id);
 
-            var reply = HandleSystemMessage(activity);
-            await connector.Conversations.ReplyToActivityAsync(reply);
+            // 如果不在真人聯絡清單中，就採用自動回覆
+            if (strIsConnect == null)
+            {
+                if (activity.GetActivityType() == ActivityTypes.Message)
+                {
+                    await Conversation.SendAsync(activity, () => new BasicQnAMakerDialog());
+                }
+
+                var reply = HandleSystemMessage(activity);
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
 
             return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
         }
@@ -49,7 +57,7 @@ namespace Microsoft.Bot.Sample.QnABot
                 // Handle conversation state changes, like members being added and removed
                 // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
                 // Not available in all channels
-
+                var a = message;
                 var reply = message.CreateReply("有任何簡單的問題都可以點下面的選項查看解答喔");
                 reply.Type = ActivityTypes.Message;
                 reply.TextFormat = TextFormatTypes.Plain;
@@ -64,6 +72,7 @@ namespace Microsoft.Bot.Sample.QnABot
                         new CardAction(){ Title = "總公司的位置", Type=ActionTypes.ImBack, Value="總公司的位置" },
                         new CardAction(){ Title = "各門市的地址與資訊可以從那裡取得", Type=ActionTypes.ImBack, Value="各門市的地址與資訊可以從那裡取得" },
                         new CardAction(){ Title = "請問運費或是手續費該如何計算", Type=ActionTypes.ImBack, Value="請問運費或是手續費該如何計算" },
+                        new CardAction(){ Title = "我想連絡小編", Type=ActionTypes.ImBack, Value="我想連絡小編" },
                     }
                 };
 
@@ -71,13 +80,20 @@ namespace Microsoft.Bot.Sample.QnABot
             }
             else if (message.Type == ActivityTypes.Message)
             {
-                var reply = message.CreateReply("請點選下方的快速連結問題內容");
-                reply.Type = ActivityTypes.Message;
-                reply.TextFormat = TextFormatTypes.Plain;
-
-                reply.SuggestedActions = new SuggestedActions()
+                // 如果是想跟小編講話，就先暫時把conversationId放入到快取清單中，並且不回覆任何訊息
+                if (message.Text == "我想連絡小編")
                 {
-                    Actions = new List<CardAction>()
+                    memoryCache.Set(message.Conversation.Id, "1", DateTimeOffset.UtcNow.AddDays(1));
+                }
+                else
+                {
+                    var reply = message.CreateReply("請點選下方的快速連結問題內容");
+                    reply.Type = ActivityTypes.Message;
+                    reply.TextFormat = TextFormatTypes.Plain;
+
+                    reply.SuggestedActions = new SuggestedActions()
+                    {
+                        Actions = new List<CardAction>()
                     {
                         new CardAction(){ Title = "如何進行退貨", Type=ActionTypes.ImBack, Value="如何進行退貨" },
                         new CardAction(){ Title = "如何取貨", Type=ActionTypes.ImBack, Value="如何取貨" },
@@ -85,10 +101,12 @@ namespace Microsoft.Bot.Sample.QnABot
                         new CardAction(){ Title = "總公司的位置", Type=ActionTypes.ImBack, Value="總公司的位置" },
                         new CardAction(){ Title = "各門市的地址與資訊可以從那裡取得", Type=ActionTypes.ImBack, Value="各門市的地址與資訊可以從那裡取得" },
                         new CardAction(){ Title = "請問運費或是手續費該如何計算", Type=ActionTypes.ImBack, Value="請問運費或是手續費該如何計算" },
-                    }
-                };
+                        new CardAction(){ Title = "我想連絡小編", Type=ActionTypes.ImBack, Value="我想連絡小編" },
+                        }
+                    };
 
-                return reply;
+                    return reply;
+                }
             }
             else if (message.Type == ActivityTypes.ContactRelationUpdate)
             {
--- Dialogs/BasicQnAMakerDialog.cs
+++ Dialogs/BasicQnAMakerDialog.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Configuration;
-
+using System.Threading.Tasks;
 using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Linq;
 
 namespace Microsoft.Bot.Sample.QnABot
 {
@@ -13,14 +16,45 @@ namespace Microsoft.Bot.Sample.QnABot
         public BasicQnAMakerDialog() : base(
             new QnAMakerService(
                 new QnAMakerAttribute(
-                    ConfigurationManager.AppSettings["QnASubscriptionKey"], 
-                    ConfigurationManager.AppSettings["QnAKnowledgebaseId"], 
-                    "can't find any answers"
+                    ConfigurationManager.AppSettings["QnASubscriptionKey"],
+                    ConfigurationManager.AppSettings["QnAKnowledgebaseId"],
+                    ""
                 )
             )
         )
         {
             // If you're running this bot locally, make sure you have these appSettings in youe web.config
+        }
+
+        protected override async Task QnAFeedbackStepAsync(IDialogContext context, QnAMakerResults qnaMakerResults)
+        {
+            // responding with the top answer when score is above some threshold
+            if (qnaMakerResults.Answers.Count > 0 && qnaMakerResults.Answers.FirstOrDefault().Score > 0.75)
+            {
+                await context.PostAsync(qnaMakerResults.Answers.FirstOrDefault().Answer);
+            }
+            else
+            {
+                await base.QnAFeedbackStepAsync(context, qnaMakerResults);
+            }
+        }
+
+
+        protected override bool IsConfidentAnswer(QnAMakerResults qnaMakerResults)
+        {
+            return base.IsConfidentAnswer(qnaMakerResults);
+        }
+
+        protected override Task DefaultWaitNextMessageAsync(IDialogContext context, IMessageActivity message, QnAMakerResults result)
+        {
+            if (result.Answers.Count == 0)
+            {
+                return base.DefaultWaitNextMessageAsync(context, message, result);
+            }
+            else
+            {
+                return base.DefaultWaitNextMessageAsync(context, message, result);
+            }
         }
     }
 }
