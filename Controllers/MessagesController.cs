8ebe9404c59b037c2ad76e3021b511c34efd93ea
maduka <maduka_bor@hotmail.com>
2017-12-07 10:20:35 +0800

    修正傳入null的問題

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -49,7 +49,7 @@ namespace Microsoft.Bot.Sample.QnABot
         private Activity HandleSystemMessage(Activity message)
         {
             // 把這次的對話加到資料庫中作記錄
-            new ConversationObj().Add(message.Conversation.Id, message.Conversation.Name, message.ChannelId);
+            new ConversationObj().Add(message.Conversation.Id, "", message.ChannelId);
 
             if (message.Type == ActivityTypes.DeleteUserData)
             {
