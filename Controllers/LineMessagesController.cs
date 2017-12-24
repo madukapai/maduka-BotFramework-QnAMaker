8260de9f990edf51edb89d1c8e90644539fdb03c
maduka <maduka_bor@hotmail.com>
2017-12-24 14:55:38 +0800

    加入LINE驗證與回音內容

--- Controllers/LineMessagesController.cs
+++ Controllers/LineMessagesController.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Newtonsoft.Json;
 using System.Configuration;
+using QnABot.Models;
 
 namespace QnABot.Controllers
 {
@@ -24,23 +25,99 @@ namespace QnABot.Controllers
         }
 
         [HttpPost]
-        public HttpResponseMessage Post()
+        [Attributes.LineSignature]
+        public IHttpActionResult Post([FromBody] LineModel.LineMessage data)
         {
-            //Get  Post RawData
-            string postData = Request.Content.ReadAsStringAsync().Result;
+            if (data == null) return BadRequest();
+            if (data.events == null) return BadRequest();
 
-            //取得LineBot接收到的訊息
-            var ReceivedMessage = LineBotHelper.GetReceivedMessage(postData);
+            foreach (LineModel.LineMessage.Event e in data.events)
+            {
+                if (e.type == LineModel.LineMessage.EventType.message)
+                {
+                    LineModel.LineReply rb = new LineModel.LineReply()
+                    {
+                        replyToken = e.replyToken,
+                        messages = procMessage(e.message)
+                    };
+                    Reply reply = new Reply(rb);
+                    reply.send();
 
-            //發送訊息
-            //var ret = LineBotHelper.SendMessage(
-            //    new List<string>() { ReceivedMessage.result[0].content.from },
-            //        "你剛才說了 " + ReceivedMessage.result[0].content.text);
+                }
+            }
+            return Ok(data);
+        }
 
-            return Request.CreateResponse(HttpStatusCode.OK);
-            
-            //如果給200，LineBot訊息就不會重送
-            //return Request.CreateResponse(HttpStatusCode.OK, ret);
+
+        private List<LineModel.SendMessage> procMessage(LineModel.ReceiveMessage m)
+        {
+            List<LineModel.SendMessage> msgs = new List<LineModel.SendMessage>();
+            LineModel.SendMessage sm = new LineModel.SendMessage()
+            {
+                type = Enum.GetName(typeof(LineModel.LineMessage.MessageType), m.type)
+            };
+            switch (m.type)
+            {
+                case LineModel.LineMessage.MessageType.sticker:
+                    sm.packageId = m.packageId;
+                    sm.stickerId = m.stickerId;
+                    break;
+                case LineModel.LineMessage.MessageType.text:
+                    sm.text = m.text;
+                    break;
+                default:
+                    sm.type = Enum.GetName(typeof(LineModel.LineMessage.MessageType), LineModel.LineMessage.MessageType.text);
+                    sm.text = "很抱歉，我只是一隻回音機器人，目前只能回覆基本貼圖與文字訊息喔！";
+                    break;
+            }
+            msgs.Add(sm);
+            return msgs;
+        }
+
+
+        public class Reply
+        {
+            public const string API_URL = "https://api.line.me/v2/bot/message/reply";
+            private WebRequest req;
+
+            public Reply(LineModel.LineReply body)
+            {
+                //--- set header and body required infos ---
+                req = WebRequest.Create(API_URL);
+                req.Method = "POST";
+                req.ContentType = "application/json";
+                req.Headers["Authorization"] = "Bearer " + ConfigurationManager.AppSettings["LineMID"].ToString();
+
+                // --- format to json and add to request body ---
+                using (var streamWriter = new StreamWriter(req.GetRequestStream()))
+                {
+                    string data = JsonConvert.SerializeObject(body);
+                    streamWriter.Write(data);
+                    streamWriter.Flush();
+                }
+            }
+
+            /*
+                --- send message to LINE ---
+                return response data
+            */
+            public string send()
+            {
+                string result = null;
+                try
+                {
+                    WebResponse response = req.GetResponse();
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        result = streamReader.ReadToEnd();
+                    }
+                }
+                catch (WebException ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                return result;
+            }
         }
     }
 }
